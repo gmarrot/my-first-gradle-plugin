6d16430841a00ddf9ead32ce6326a48cff5ca46a
Gaëtan Marrot <gaetan.marrot@gmail.com>
2018-01-07 19:13:48 +0100

    Info task refactoring

--- src/main/groovy/com/betomorrow/gradle/sample/MyFirstPlugin.groovy
+++ src/main/groovy/com/betomorrow/gradle/sample/MyFirstPlugin.groovy
@@ -1,36 +1,20 @@
 package com.betomorrow.gradle.sample
 
-import groovy.json.JsonBuilder
-import org.ajoberstar.grgit.Grgit
+import com.betomorrow.gradle.sample.extensions.InfoExtension
+import com.betomorrow.gradle.sample.tasks.InfoTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-import java.time.Instant
 
 class MyFirstPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
         project.with {
-            def info = extensions.create("info", InfoExtension)
+            def info = extensions.create("info", InfoExtension, project)
 
-            task("info", description: "Show project information", group: "other") {
-                doLast {
-                    def grgit = Grgit.open()
-
-                    def builder = new JsonBuilder()
-
-                    def root = builder.metadata {}
-                    root.metadata.name = project.name
-                    root.metadata.version = project.version
-                    root.metadata.buildDate = Instant.now().toString()
-                    root.metadata.revision = grgit.log().first().getAbbreviatedId()
-                    root.changelog = grgit.log(maxCommits: info.logSize).collect { it.fullMessage }
-
-                    project.file(info.filename).withWriter {
-                        it.write(builder.toPrettyString())
-                    }
-                }
+            task("info", description: "Write project information", group: "other", type: InfoTask) {
+                filePath = info.filename
+                logSize = info.logSize
             }
         }
     }
--- src/test/groovy/com/betomorrow/gradle/sample/MyFirstPluginTest.groovy
+++ src/test/groovy/com/betomorrow/gradle/sample/MyFirstPluginTest.groovy
@@ -1,5 +1,6 @@
 package com.betomorrow.gradle.sample
 
+import com.betomorrow.gradle.sample.tasks.InfoTask
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
@@ -12,6 +13,33 @@ class MyFirstPluginTest extends Specification {
 
         then:
         project.tasks.info != null
+    }
+
+    def "test info task uses default property values"() {
+        when:
+        def project = ProjectBuilder.builder().build()
+        project.apply plugin: 'com.betomorrow.my-first-gradle-plugin'
+
+        then:
+        def infoTask = (InfoTask) project.tasks.info
+        infoTask.filePath.get() == "info.json"
+        infoTask.logSize.get() == 10
+    }
+
+    def "test info task properties can be overriden"() {
+        when:
+        def project = ProjectBuilder.builder().build()
+        project.apply plugin: 'com.betomorrow.my-first-gradle-plugin'
+
+        project.info {
+            filename = "package.json"
+            logSize = 1
+        }
+
+        then:
+        def infoTask = (InfoTask) project.tasks.info
+        infoTask.filePath.get() == "package.json"
+        infoTask.logSize.get() == 1
     }
 
 }
